08e34dd55cc7c8a997314c06bc2333ba8d28eccc
jiraynor <jiraynor@naver.com>
2023-07-25 11:51:58 +0900

    2023.07.25. 11:50

--- Board/src/com/seojihoon/board/repository/UserRepository.java
+++ Board/src/com/seojihoon/board/repository/UserRepository.java
@@ -10,6 +10,10 @@ public interface UserRepository extends Repository<User, String> {
 	// 유저 정보를 저장할 User Entity List
 	public static final List<User> USERS = new ArrayList<User>();
 	
+	public abstract boolean existsByEmail(String email);
+	public abstract boolean existsByTelNumber(String telNumber);
+	public abstract boolean existsByNickname(String nickname);
+	
 }
 
 
